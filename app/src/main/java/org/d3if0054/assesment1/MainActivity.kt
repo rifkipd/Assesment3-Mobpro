528b85095379431df769d6a42315035368307668
Rifki Padilah <rpadilah90@gmail.com>
2022-03-30 22:07:07 +0700

    Menambahkan Proses pada inputan

--- app/src/main/java/org/d3if0054/assesment1/MainActivity.kt
+++ app/src/main/java/org/d3if0054/assesment1/MainActivity.kt
@@ -1,8 +1,11 @@
 package org.d3if0054.assesment1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
 import org.d3if0054.assesment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +16,62 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.fpbButton.setOnClickListener{(hitungFpb())}
+        binding.kpkButton.setOnClickListener{(hitungKpk())}
+    }
+
+    private fun hitungKpk(){
+        //mengambil angka dari inputan user
+        val angkaSatu = binding.angkaSatuText.text.toString()
+        val angkaDua = binding.angkaDuaText.text.toString()
+
+        //merubah angka menjadi int
+        val first = angkaSatu.toInt()
+        val second = angkaDua.toInt()
+
+        //pengecekan angka ke 1 dan ke 2 yang akan dimasukan kedalam variable kpk
+        var kpk = if(first > second)
+            first
+        else
+            second
+
+
+        //proses perhitungan untuk mencari nilai kpk
+        while (true) {
+            if ((kpk % first == 0) && (kpk % second == 0)) {
+                val finalSum = kpk.toString()
+                binding.hasilTextView.text = getString(R.string.hasilText_kpk,finalSum)
+                break
+            }
+            ++kpk
+        }
+
+    }
+
+    private fun hitungFpb(){
+        //mengambil angka dari yg di inputkan user
+        val angkaSatu = binding.angkaSatuText.text.toString()
+        val angkaDua = binding.angkaDuaText.text.toString()
+
+
+        //angka di rubah dari string ke int
+        var first = angkaSatu.toInt()
+        var second = angkaDua.toInt()
+
+
+        //proses perhitungan untuk mencari nilai FPB
+        while (first != second){
+            if (first > second) {
+                first -= second
+            }else{
+                second -= first
+            }
+        }
+
+        //hasil di rubah lagi menjadi string
+        val finalSum = first.toString()
+        binding.hasilTextView.text = getString(R.string.hasilText_fpb,finalSum)
+
     }
 }
