252c6589c286b0cc116004249fd311460f5014e2
Rifki Padilah <rpadilah90@gmail.com>
2022-05-25 23:07:24 +0700

    ASSESMENT 2

--- app/src/main/java/org/d3if0054/assesment1/MainActivity.kt
+++ app/src/main/java/org/d3if0054/assesment1/MainActivity.kt
@@ -3,92 +3,42 @@ package org.d3if0054.assesment1
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.text.TextUtils
+import android.util.Log
+import android.view.LayoutInflater
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import org.d3if0054.assesment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.fpbButton.setOnClickListener{(hitungFpb())}
-        binding.kpkButton.setOnClickListener{(hitungKpk())}
-    }
-
-    private fun hitungKpk(){
-        //mengambil angka dari inputan user
-        val angkaSatu = binding.angkaSatuText.text.toString()
-        if(TextUtils.isEmpty(angkaSatu)){
-            Toast.makeText(this,R.string.angka_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val angkaDua = binding.angkaDuaText.text.toString()
-        if(TextUtils.isEmpty(angkaDua)){
-            Toast.makeText(this,R.string.angka_invalid, Toast.LENGTH_LONG).show()
-            return
-
+        binding.goBtn1.setOnClickListener {
+            replaceFragment(HitungFaktorFragment())
         }
 
-
-        //merubah angka menjadi int
-        val first = angkaSatu.toInt()
-        val second = angkaDua.toInt()
-
-        //pengecekan angka ke 1 dan ke 2 yang akan dimasukan kedalam variable kpk
-        var kpk = if(first > second)
-            first
-        else
-            second
-
-
-        //proses perhitungan untuk mencari nilai kpk
-        while (true) {
-            if ((kpk % first == 0) && (kpk % second == 0)) {
-                val finalSum = kpk.toString()
-                binding.hasilTextView.text = getString(R.string.hasilText_kpk,finalSum)
-                break
-            }
-            ++kpk
+        binding.goBtn2.setOnClickListener {
+            replaceFragment(HitungFragment())
         }
+
+        binding.goBtn3.setOnClickListener {
+            replaceFragment(HitungPrimaFragment())
+        }
+
 
     }
 
-    private fun hitungFpb(){
-        //mengambil angka dari yg di inputkan user
-        val angkaSatu = binding.angkaSatuText.text.toString()
-        if(TextUtils.isEmpty(angkaSatu)){
-            Toast.makeText(this,R.string.angka_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-        val angkaDua = binding.angkaDuaText.text.toString()
-        if(TextUtils.isEmpty(angkaDua)){
-            Toast.makeText(this,R.string.angka_invalid, Toast.LENGTH_LONG).show()
-            return
-        }
-
-
-        //angka di rubah dari string ke int
-        var first = angkaSatu.toInt()
-        var second = angkaDua.toInt()
-
-
-        //proses perhitungan untuk mencari nilai FPB
-        while (first != second){
-            if (first > second) {
-                first -= second
-            }else{
-                second -= first
-            }
-        }
-
-        //hasil di rubah lagi menjadi string
-        val finalSum = first.toString()
-        binding.hasilTextView.text = getString(R.string.hasilText_fpb,finalSum)
-
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer,fragment)
+        fragmentTransaction.commit()
     }
 }
